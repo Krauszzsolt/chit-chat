ad1ab059ab7a314b623694825b12c10d57fc1f52
Krausz Zsolt <krausz.zsolt97@gmail.com>
2021-03-05 15:33:23 +0100

    add interface

--- backend/BLL/Services/Interfaces/IMessageService.cs
+++ backend/BLL/Services/Interfaces/IMessageService.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using BLL.DTOs.Message;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BLL.Services.Interfaces
 {
     public interface IMessageService
     {
+        public Task<MessageDto> GetMessage(Guid id);
+
+        public Task<MessageListDto> GetMessages(Guid chatroomId);
+
+        public Task PostMessage(MessageDto messageDto);
+
+        public Task PutMessage(MessageDto messageDto);
+
+        public Task DeleteMessage(Guid id, string userId);
+
     }
 }
