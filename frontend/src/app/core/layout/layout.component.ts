f929db521c2cbd9b4bec5656192d6d9ca6975260
Krausz Zsolt <krausz.zsolt97@gmail.com>
2021-10-23 13:22:30 +0200

    profile picture sitebuild

--- frontend/src/app/core/layout/layout.component.ts
+++ frontend/src/app/core/layout/layout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { InviteDialogComponent } from 'src/app/feature/profile/invite-dialog/invite-dialog.component';
+import { UploadPictureDialogComponent } from 'src/app/feature/profile/upload-picture-dialog/upload-picture-dialog.component';
 import { ApplicationUserDto } from 'src/app/shared/client';
 import { AuthService } from '../service/auth.service';
 @Component({
@@ -27,5 +28,13 @@ export class LayoutComponent implements OnInit {
   }
   public logout() {
     this.authService.logout();
+  }
+
+  public uploadPicture() {
+    const dialogRef = this.dialog.open(UploadPictureDialogComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 }
